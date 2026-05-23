e8c1cd9d06d7e4bf964afffd53e24201a85512bd
Marguerite68 <sekaibest@outlook.com>
2026-05-23 10:53:02 +0800

    add BootScreen composable with background image and text elements

--- app/src/main/java/com/example/madodict/bootSreeen.kt
+++ app/src/main/java/com/example/madodict/bootSreeen.kt
@@ -1,2 +1,103 @@
 package com.example.madodict
 
+import android.R.attr.letterSpacing
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.madodict.ui.theme.LightTitleEn
+import com.example.madodict.ui.theme.LightTitleZh
+import com.example.madodict.ui.theme.SplashTitleEn
+import com.example.madodict.ui.theme.SplashTitleZh
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.madodict.ui.theme.LightBottomText
+import com.example.madodict.ui.theme.SplashFooter
+
+@Composable
+fun BootScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.main_kv02_modified),
+            contentDescription = "boot screen background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize().padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.main_kv_chara),
+                contentDescription = "boot screen character",
+                modifier = Modifier.height(550.dp)
+            )
+            val titleShadow = Shadow(
+                color = Color.Black.copy(alpha = 0.35f),
+                offset = Offset(2f, 2f),
+                blurRadius = 3.5f
+            )
+            Text(
+                text = "魔法字典",
+                style = SplashTitleZh.copy(shadow = titleShadow),
+                color = LightTitleZh,
+                letterSpacing = 2.sp
+            )
+            Text(
+                text = "Madoka runes dictionary",
+                style = SplashTitleEn.copy(shadow = titleShadow),
+                color = LightTitleEn,
+                letterSpacing = 1.1.sp
+            )
+        }
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 20.dp)
+        ) {
+            Text(
+                text = "This application follows the CC BY-NC 4.0 license",
+                style = SplashFooter,
+                color = LightBottomText,
+                letterSpacing = 0.5.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text = "and is open-sourced on https://github.com/Marguerite68/Mado_Dict",
+                style = SplashFooter,
+                color = LightBottomText,
+                letterSpacing = 0.5.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BootScreenPreview() {
+    BootScreen()
+}
