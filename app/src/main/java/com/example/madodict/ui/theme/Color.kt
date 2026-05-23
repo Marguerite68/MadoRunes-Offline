67c281fe54bac305d30f6de748a2464459e9a885
Marguerite68 <sekaibest@outlook.com>
2026-05-23 10:09:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/madodict/ui/theme/Color.kt
@@ -0,0 +1,45 @@
+package com.example.madodict.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light mode
+val LightBackground = Color(0xFFFFF5F7)
+val LightOnBackground = Color(0xFF2D141A)
+val LightOutline = Color(0xFFFFB7C5)
+val LightOutlineVariant = Color(0xFFFFE2E8)
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnSurface = Color(0xFF2D141A)
+val LightPrimary = Color(0xFFFF6B8B)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightSecondary = Color(0xFFFFEAA7)
+val LightOnSecondary = Color(0xFF2D141A)
+val LightPrimaryContainer = Color(0xFFFFF0F3)
+val LightOnPrimaryContainer = Color(0xFF2D141A)
+val LightMuted = Color(0xFFA69498)
+val LightTitleZh = Color(0xFFEE8CAC)
+val LightTitleEn = Color(0xFFEBB38B)
+val LightBottomText = Color(0xFF595959)
+val LightHint = Color(0x2E000000) // 18% black
+val LightTableFillEmphasis = Color(0xFFFFF5F7)
+val LightTableFillNormal = Color(0xFF000000)
+
+// Dark mode
+val DarkBackground = Color(0xFF0D0B12)
+val DarkOnBackground = Color(0xFFE6E1EC)
+val DarkOutline = Color(0xFF6E6488)
+val DarkOutlineVariant = Color(0xFF312A3F)
+val DarkSurface = Color(0xFF16121E)
+val DarkOnSurface = Color(0xFFE6E1EC)
+val DarkPrimary = Color(0xFFA18CD1)
+val DarkOnPrimary = Color(0xFF000000)
+val DarkSecondary = Color(0xFFA18CD1)
+val DarkOnSecondary = Color(0xFF000000)
+val DarkPrimaryContainer = Color(0xFF2D253F)
+val DarkOnPrimaryContainer = Color(0xFFE6E1EC)
+val DarkMuted = Color(0xFF5A5266)
+val DarkTitleZh = Color(0xFFEE8CAC)
+val DarkTitleEn = Color(0xFFEBB38B)
+val DarkBottomText = Color(0xFF595959)
+val DarkHint = Color(0x4DE6E1EC) // 30% white
+val DarkTableFillEmphasis = Color(0xFF0D0B12)
+val DarkTableFillNormal = Color(0xFF000000)
---
+++ app/src/main/java/com/example/madodict/ui/theme/Type.kt
@@ -0,0 +1,100 @@
+package com.example.madodict.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.madodict.R
+
+private val Huitian = FontFamily(Font(R.font.huitian))
+private val HanKengGroteskLight = FontFamily(Font(R.font.hankengrotesk_light))
+private val FzLantingYuans = FontFamily(Font(R.font.fzlantingyuans_l_gb))
+private val MadokaArchaic = FontFamily(Font(R.font.madoka_archaic))
+private val MadokaModren = FontFamily(Font(R.font.madoka_modren))
+private val MadokaMusical = FontFamily(Font(R.font.madoka_musical))
+private val MagicumTexturae = FontFamily(Font(R.font.magicum_texturae))
+
+val SplashTitleZh = TextStyle(
+    fontFamily = Huitian,
+    fontWeight = FontWeight.Normal,
+    fontSize = 69.sp
+)
+val SplashTitleEn = TextStyle(
+    fontFamily = Huitian,
+    fontWeight = FontWeight.Normal,
+    fontSize = 15.sp
+)
+val SplashFooter = TextStyle(
+    fontFamily = HanKengGroteskLight,
+    fontWeight = FontWeight.Normal,
+    fontSize = 6.sp
+)
+
+val PageTitle = TextStyle(
+    fontFamily = FzLantingYuans,
+    fontWeight = FontWeight.Normal,
+    fontSize = 30.sp
+)
+val PageBodyText = TextStyle(
+    fontFamily = FzLantingYuans,
+    fontWeight = FontWeight.Normal,
+    fontSize = 10.sp
+)
+val ContrastHanLatinText = TextStyle(
+    fontFamily = FzLantingYuans,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp
+)
+val ContrastArchaicText = TextStyle(
+    fontFamily = MadokaArchaic,
+    fontWeight = FontWeight.Normal,
+    fontSize = 20.sp
+)
+val ContrastModrenText = TextStyle(
+    fontFamily = MadokaModren,
+    fontWeight = FontWeight.Normal,
+    fontSize = 20.sp
+)
+val ContrastMusicalText = TextStyle(
+    fontFamily = MadokaMusical,
+    fontWeight = FontWeight.Normal,
+    fontSize = 20.sp
+)
+val ContrastGothicText = TextStyle(
+    fontFamily = MagicumTexturae,
+    fontWeight = FontWeight.Normal,
+    fontSize = 20.sp
+)
+val SettingLabelText = TextStyle(
+    fontFamily = FzLantingYuans,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp
+)
+val DropdownAndColorInputText = TextStyle(
+    fontFamily = FzLantingYuans,
+    fontWeight = FontWeight.Normal,
+    fontSize = 11.sp
+)
+val LanguageNameText = TextStyle(
+    fontFamily = HanKengGroteskLight,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp
+)
+val InfoAndBottomBarLabelText = TextStyle(
+    fontFamily = FzLantingYuans,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    titleLarge = PageTitle,
+    bodyLarge = PageBodyText,
+    bodyMedium = ContrastHanLatinText,
+    bodySmall = PageBodyText,
+    labelLarge = PageBodyText,
+    labelMedium = SettingLabelText,
+    labelSmall = DropdownAndColorInputText
+)
